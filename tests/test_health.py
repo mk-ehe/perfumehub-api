faf631a4908e90014d428fca668f3001a5ae3413
MK-EHE <kontakt.mateusz.kudas@gmail.com>
2026-04-30 23:25:09 +0200

    added tests for /search, added auto limiter reset

--- tests/test_health.py
+++ tests/test_health.py
@@ -1,26 +1,12 @@
 from fastapi.testclient import TestClient
 from main import app
+import pytest
 
 client = TestClient(app)
 
-def test_ping():
-    response = client.get("/ping")
-
-    assert response.status_code == 200
-    assert response.json() == {"status": "ok"}
-
-
-def test_ping_limit_exceeded():
+@pytest.fixture(autouse=True)
+def reset_rate_limiter():
     app.state.limiter.reset()
-
-    for _ in range(20):
-        response = client.get("/ping")
-        assert response.status_code == 200
-
-    limit_exceeded = client.get("/ping")
-    assert limit_exceeded.status_code == 429
-    assert "rate limit exceeded" in limit_exceeded.json()["error"].lower()
-
 
 def test_guide():
     response = client.get("/")
@@ -28,3 +14,18 @@ def test_guide():
     assert response.status_code == 200
     assert response.json()["author"] == "mk-ehe"
     assert "routes" in response.json()
+
+def test_ping():
+    response = client.get("/ping")
+
+    assert response.status_code == 200
+    assert response.json() == {"status": "ok"}
+
+def test_ping_limit_exceeded():
+    for _ in range(20):
+        response = client.get("/ping")
+        assert response.status_code == 200
+
+    limit_exceeded = client.get("/ping")
+    assert limit_exceeded.status_code == 429
+    assert "rate limit exceeded" in limit_exceeded.json()["error"].lower()
---
+++ tests/test_search.py
@@ -0,0 +1,60 @@
+from fastapi.testclient import TestClient
+from main import app
+import pytest
+
+client = TestClient(app)
+
+@pytest.fixture(autouse=True)
+def reset_rate_limiter():
+    app.state.limiter.reset()
+
+def test_search():
+    params = {"url": "https://perfumehub.pl/dior-sauvage-woda-toaletowa-dla-mezczyzn-100-ml"}
+    response = client.get("/search", params=params)
+
+    data = response.json()
+
+    assert response.status_code == 200
+    assert "subscribers" not in data
+    assert "_id" not in data
+    assert data["url"] != ""
+    assert data["fragrance"] != ""
+
+
+def test_search_invalid_domain():
+    wrong_params = {"url": "https://allegro.pl/dior/sauvage"}
+    response = client.get("/search", params=wrong_params)
+
+    assert response.status_code == 400
+    assert response.json()["detail"].lower() == "Invalid domain. Only official Perfumehub URLs are allowed.".lower()
+
+def test_search_malformed_url():
+    wrong_params = {"url": "https://[::1/some-perfumes"}
+    response = client.get("/search", params=wrong_params)
+
+    assert response.status_code == 400
+    assert response.json()["detail"] == "Malformed URL provided."
+
+def test_search_invalid_path():
+    wrong_params = {"url": "perfumehub.pl/fake/perfume/path"}
+    response = client.get("/search", params=wrong_params)
+
+    assert response.status_code == 500
+    assert response.json()["detail"].lower() == "An error occurred while fetching the price.".lower()
+
+def test_search_no_url():
+    wrong_params = {"url": ""}
+    response = client.get("/search", params=wrong_params)
+
+    assert response.status_code == 400
+    assert response.json()["detail"].lower() == "Invalid domain. Only official Perfumehub URLs are allowed.".lower()
+
+def test_search_limit_exceeded():
+    params = {"url": "https://perfumehub.pl/dior-sauvage-woda-toaletowa-dla-mezczyzn-100-ml"}
+    response = client.get("/search", params=params)
+    assert response.status_code == 200
+
+    limit_exceeded = client.get("/search", params=params)
+    assert limit_exceeded.status_code == 429
+    assert "rate limit exceeded" in limit_exceeded.json()["error"].lower()
+ 
